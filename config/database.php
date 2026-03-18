5e4f5386c3ac5a2f3ed60d6543a62fd7ccc8afaf
Luis Enrique Álvarez <luenalgo@gmail.com>
2026-03-18 13:56:25 -0600

    chore: setup cpanel git deployment and ignore config

--- config/database.php
+++
@@ -1,7 +0,0 @@
-<?php
-return [
-    'host' => 'dedi-1675829.tjaech.gob.mx',
-    'database' => 'tjaechgob_constancias_tja',
-    'username' => 'tjaechgob_constancias_usr',
-    'password' => 'Chiapas2026TA/**',
-];
